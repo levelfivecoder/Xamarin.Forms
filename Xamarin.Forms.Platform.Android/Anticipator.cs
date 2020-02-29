75cd6e2b2f4b6fca4f1a730144b226bace7a3814
Chris King <kingces95@gmail.com>
2020-02-28 17:13:58 -1000

    warehouse

--- Xamarin.Forms.Platform.Android/Anticipator.cs
+++ Xamarin.Forms.Platform.Android/Anticipator.cs
@@ -10,7 +10,11 @@ using ABuildVersionCodes = Android.OS.BuildVersionCodes;
 using ABuild = Android.OS.Build;
 using AView = Android.Views.View;
 using ARelativeLayout = Android.Widget.RelativeLayout;
-//using AToolbar = Android.Support.V7.Widget.Toolbar;
+#if __ANDROID_29__
+using AToolbar = AndroidX.AppCompat.Widget.Toolbar;
+#else
+using AToolbar = Android.Support.V7.Widget.Toolbar;
+# endif
 using System.Threading;
 using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
@@ -21,16 +25,21 @@ using System.Linq;
 
 namespace Xamarin.Forms.Platform.Android
 {
-	public interface IAnticipatable
+	interface IPreBuildable
 	{
-		object Get();
+		object Build();
+	}
+
+	interface IPreComputable
+	{
+		object Compute();
 	}
 
 	public sealed class AndroidAnticipator
 	{
 		static class Key
 		{
-			internal struct ClassConstruction : IAnticipatable
+			internal struct ClassConstruction : IPreComputable
 			{
 				private Type _type;
 
@@ -39,7 +48,7 @@ namespace Xamarin.Forms.Platform.Android
 					_type = type;
 				}
 
-				object IAnticipatable.Get()
+				object IPreComputable.Compute()
 				{
 					RuntimeHelpers.RunClassConstructor(_type.TypeHandle);
 					return null;
@@ -51,16 +60,16 @@ namespace Xamarin.Forms.Platform.Android
 				}
 			}
 
-			internal struct SdkVersion : IAnticipatable
+			internal struct SdkVersion : IPreComputable
 			{
-				object IAnticipatable.Get()
+				object IPreComputable.Compute()
 					=> ABuild.VERSION.SdkInt;
 
 				public override string ToString()
 					=> $"{nameof(SdkVersion)}";
 			}
 
-			internal struct IdedResourceExists : IAnticipatable
+			internal struct IdedResourceExists : IPreComputable
 			{
 				readonly internal Context Context;
 				readonly internal int Id;
@@ -71,7 +80,7 @@ namespace Xamarin.Forms.Platform.Android
 					Id = id;
 				}
 
-				object IAnticipatable.Get()
+				object IPreComputable.Compute()
 				{
 					if (Id == 0)
 						return false;
@@ -84,7 +93,7 @@ namespace Xamarin.Forms.Platform.Android
 					=> $"{nameof(IdedResourceExists)}, id={Id}, '{ResourceName(Id)}'";
 			}
 
-			internal struct NamedResourceExists : IAnticipatable
+			internal struct NamedResourceExists : IPreComputable
 			{
 				readonly internal Context Context;
 				readonly internal string Name;
@@ -97,7 +106,7 @@ namespace Xamarin.Forms.Platform.Android
 					Type = type;
 				}
 
-				object IAnticipatable.Get()
+				object IPreComputable.Compute()
 				{
 					var id = Context.Resources.GetIdentifier(Name, Type, Context.PackageName);
 					if (id == 0)
@@ -111,7 +120,7 @@ namespace Xamarin.Forms.Platform.Android
 					=> $"{nameof(NamedResourceExists)}, name='{Name}', type='{Type}'";
 			}
 
-			internal struct InflateResource : IAnticipatable
+			internal struct InflateResource : IPreBuildable
 			{
 				readonly internal Context Context;
 				readonly internal int Id;
@@ -122,47 +131,23 @@ namespace Xamarin.Forms.Platform.Android
 					Id = id;
 				}
 
-				object IAnticipatable.Get()
+				object IPreBuildable.Build()
 				{
 					if (Id == 0)
 						return null;
 
-					var activity = (Activity)Context;
+					var layoutInflator = (Context as Activity)?.LayoutInflater ?? 
+						LayoutInflater.FromContext(Context);
 
-					var layoutInflator = activity.LayoutInflater;
 					return layoutInflator.Inflate(Id, null);
 				}
 
 				public override string ToString()
-					=> $"{nameof(InflateResource)}, id={Id}, '{ResourceName(Id)}'";
-			}
-
-			internal struct InflateIdedResourceFromContext : IAnticipatable
-			{
-				readonly internal Context Context;
-				readonly internal int Id;
-
-				internal InflateIdedResourceFromContext(Context context, int id)
-				{
-					Context = context;
-					Id = id;
-				}
-
-				object IAnticipatable.Get()
-				{
-					if (Id == 0)
-						return null;
-
-					var layoutInflator = LayoutInflater.FromContext(Context);
-					return layoutInflator.Inflate(Id, null);
-				}
-
-				public override string ToString()
-					=> $"{nameof(InflateResource)}, id={Id}, '{ResourceName(Id)}'";
+					=> $"{nameof(InflateResource)}, id={ResourceName(Id)}";
 			}
 
 			internal struct ActivateView :
-				IAnticipatable, IEquatable<ActivateView>
+				IPreBuildable, IEquatable<ActivateView>
 			{
 				readonly internal Context Context;
 				readonly internal Type Type;
@@ -175,7 +160,7 @@ namespace Xamarin.Forms.Platform.Android
 					Factory = activator;
 				}
 
-				object IAnticipatable.Get()
+				object IPreBuildable.Build()
 				{
 					if (Factory == null)
 						return Activator.CreateInstance(Type, Context);
@@ -204,14 +189,14 @@ namespace Xamarin.Forms.Platform.Android
 			s_singleton.AnticipateValue(new Key.ClassConstruction(typeof(Resource.Layout)));
 			s_singleton.AnticipateValue(new Key.ClassConstruction(typeof(Resource.Attribute)));
 
-			//s_singleton.AnticipateAllocation(new Key.ActivateView(context, typeof(AToolbar), o => new AToolbar(o)));
+			s_singleton.AnticipateAllocation(new Key.ActivateView(context, typeof(AToolbar), o => new AToolbar(o)));
 			s_singleton.AnticipateAllocation(new Key.ActivateView(context.BaseContext, typeof(ARelativeLayout), o => new ARelativeLayout(o)));
 			s_singleton.AnticipateAllocation(new Key.InflateResource(context, FormsAppCompatActivity.ToolbarResource));
 
 			s_singleton.AnticipateValue(new Key.IdedResourceExists(context, global::Android.Resource.Attribute.ColorAccent));
 			s_singleton.AnticipateValue(new Key.NamedResourceExists(context, "colorAccent", "attr"));
 
-			s_singleton.AnticipateAllocation(new Key.InflateIdedResourceFromContext(context, Resource.Layout.FlyoutContent));
+			s_singleton.AnticipateAllocation(new Key.InflateResource(context, Resource.Layout.FlyoutContent));
 
 			//s_singleton.AnticipateAllocation(new Key.ActivateView(context, typeof(FLabelRenderer)));
 			//s_singleton.AnticipateAllocation(new Key.ActivateView(context, typeof(PageRenderer)));
@@ -232,19 +217,19 @@ namespace Xamarin.Forms.Platform.Android
 			=> s_singleton.Dispose();
 
 		internal static ABuildVersionCodes SdkVersion
-			=> (ABuildVersionCodes)s_singleton.Get(new Key.SdkVersion());
+			=> (ABuildVersionCodes)s_singleton.Compute(new Key.SdkVersion());
 
 		internal static bool IdedResourceExists(Context context, int id)
-			=> (bool)s_singleton.Get(new Key.IdedResourceExists(context, id));
+			=> (bool)s_singleton.Compute(new Key.IdedResourceExists(context, id));
 
 		internal static bool NamedResourceExists(Context context, string name, string type)
-			=> (bool)s_singleton.Get(new Key.NamedResourceExists(context, name, type));
+			=> (bool)s_singleton.Compute(new Key.NamedResourceExists(context, name, type));
 
 		internal static AView InflateResource(Context context, int id)
-			=> (AView)s_singleton.Get(new Key.InflateResource(context, id));
+			=> (AView)s_singleton.Allocate(new Key.InflateResource(context, id));
 
 		internal static AView ActivateView(Context context, Type type)
-			=> (AView)s_singleton.Get(new Key.ActivateView(context, type));
+			=> (AView)s_singleton.Allocate(new Key.ActivateView(context, type));
 
 		static string ResourceName(int id)
 			=> id != 0 && s_resourceNames.TryGetValue(id, out var name) ? name : id.ToString();
@@ -259,7 +244,200 @@ namespace Xamarin.Forms.Platform.Android
 		static Anticipator s_singleton = new Anticipator();
 	}
 
-	internal class Scheduler
+	sealed class Anticipator
+	{
+		sealed class Warehouse : IDisposable
+		{
+			static ConcurrentBag<object> ActivateBag(object key)
+				=> new ConcurrentBag<object>();
+
+			readonly ConcurrentDictionary<object, object> _dictionary;
+			readonly Func<object, ConcurrentBag<object>> _activateBag;
+			readonly Scheduler _scheduler;
+
+			internal Warehouse(Scheduler scheduler)
+			{
+				_activateBag = ActivateBag;
+				_dictionary = new ConcurrentDictionary<object, object>();
+				_scheduler = scheduler;
+			}
+
+			ConcurrentBag<object> Get(object key)
+				=> (ConcurrentBag<object>)_dictionary.GetOrAdd(key, _activateBag);
+
+			void Set(object key, object value)
+				=> Get(key).Add(value);
+
+			bool TryGet(object key, out object value)
+			{
+				var result = Get(key).TryTake(out value);
+				Profile.WriteLog("WAREHOUSE {0}: {1}", result ? "HIT" : "MISS", key);
+				return result;
+			}
+
+			public object Get<T>(T key = default)
+				where T : IPreBuildable
+			{
+				if (!TryGet(key, out var value))
+					return key.Build();
+
+				return value;
+			}
+
+			public void Anticipate<T>(T key = default)
+				where T : IPreBuildable
+			{
+				_scheduler.Schedule(() =>
+				{
+					try
+					{
+						var stopwatch = new Stopwatch();
+						stopwatch.Start();
+						Set(key, key.Build());
+						var ticks = stopwatch.ElapsedTicks;
+
+						Profile.WriteLog("WEARHOUSED: {0}, ms={1}", key, TimeSpan.FromTicks(ticks).Milliseconds);
+					}
+					catch (Exception ex)
+					{
+						Profile.WriteLog("WEARHOUSE EXCEPTION: {0}: {1}", key, ex);
+					}
+				});
+			}
+
+			public void Dispose()
+			{
+				foreach (var pair in _dictionary)
+				{
+					foreach (var value in ((ConcurrentBag<object>)pair.Value))
+					{
+						Profile.WriteLog("WEARHOUSE UNUSED: {0}", pair.Key);
+						(value as IDisposable)?.Dispose();
+					}
+				}
+
+				_dictionary.Clear();
+
+				Profile.WriteLog("WEARHOUSE DISPOSED");
+			}
+		}
+
+		sealed class Cache : IDisposable
+		{
+			readonly ConcurrentDictionary<object, object> _dictionary;
+			readonly ConcurrentDictionary<object, bool> _accessed;
+			readonly Scheduler _scheduler;
+
+			internal Cache(Scheduler scheduler)
+			{
+				_scheduler = scheduler;
+				_accessed = new ConcurrentDictionary<object, bool>();
+				_dictionary = new ConcurrentDictionary<object, object>();
+			}
+
+			void Set(object key, object value)
+				=> _dictionary.TryAdd(key, value);
+
+			bool TryGet(object key, out object value)
+			{
+				var result = _dictionary.TryGetValue(key, out value);
+				if (!result)
+					return false;
+
+				if (_accessed.TryAdd(key, true))
+					Profile.WriteLog("CACHE HIT: {0}", key);
+
+				return true;
+			}
+
+			public object Get<T>(T key = default)
+				where T : IPreComputable
+			{
+				if (!TryGet(key, out var value))
+					return key.Compute();
+
+				return value;
+			}
+
+			public void Anticipate<T>(T key = default)
+				where T : IPreComputable
+			{
+				_scheduler.Schedule(() =>
+				{
+					try
+					{
+						var stopwatch = new Stopwatch();
+						stopwatch.Start();
+						Set(key, key.Compute());
+						var ticks = stopwatch.ElapsedTicks;
+
+						Profile.WriteLog("CACHED: {0}, ms={1}", key, TimeSpan.FromTicks(ticks).Milliseconds);
+					}
+					catch (Exception ex)
+					{
+						Profile.WriteLog("CACHE EXCEPTION: {0}: {1}", key, ex);
+					}
+				});
+			}
+
+			public void Dispose()
+			{
+				foreach (var pair in _dictionary)
+				{
+					if (!_accessed.ContainsKey(pair.Key))
+						Profile.WriteLog("CACHE UNUSED: {0}", pair.Key);
+
+					(pair.Value as IDisposable)?.Dispose();
+				}
+
+				_dictionary.Clear();
+
+				Profile.WriteLog("CACHE DISPOSED");
+			}
+		}
+
+		readonly Scheduler _scheduler;
+		readonly Cache _cache;
+		readonly Warehouse _heap;
+		readonly CpuUsage __cpuStart;
+
+		internal Anticipator()
+		{
+			_scheduler = new Scheduler();
+			_heap = new Warehouse(_scheduler);
+			_cache = new Cache(_scheduler);
+			__cpuStart = CpuUsage.Now;
+		}
+
+		internal void AnticipateAllocation<T>(T key = default)
+			where T : IPreBuildable
+			=> _heap.Anticipate(key);
+
+		internal object Allocate<T>(T key = default)
+			where T : IPreBuildable
+			=> _heap.Get(key);
+
+		internal void AnticipateValue<T>(T key = default)
+			where T : IPreComputable
+			=> _cache.Anticipate(key);
+
+		internal object Compute<T>(T key = default)
+			where T : IPreComputable
+			=> _cache.Get(key);
+
+		public void Dispose()
+		{
+			_scheduler.Join();
+			_cache.Dispose();
+			_heap.Dispose();
+
+			var cpuNow = CpuUsage.Now;
+
+			Profile.WriteLog("ANTICIPATOR: CPU UTIL {0}%", cpuNow - __cpuStart);
+		}
+	}
+
+	sealed class Scheduler
 	{
 		private static readonly TimeSpan LoopTimeOut = TimeSpan.FromSeconds(5.0);
 		private readonly Thread _thread;
@@ -361,195 +539,6 @@ namespace Xamarin.Forms.Platform.Android
 
 		public override string ToString()
 			=> $"Total={_totalIdle.Value.Total}, Idle={_totalIdle.Value.Idle}";
-	}
-
-	internal sealed class Anticipator
-	{
-		interface IHeapOrCache : IDisposable
-		{
-			void Set(object key, object value);
-			bool TryGet(object key, out object value);
-		}
-
-		sealed class HeapOrCache : IDisposable
-		{
-			sealed class Cache : IHeapOrCache
-			{
-				readonly ConcurrentDictionary<object, object> _dictionary
-					= new ConcurrentDictionary<object, object>();
-				readonly ConcurrentDictionary<object, bool> _accessed
-					= new ConcurrentDictionary<object, bool>();
-
-				void Log(string format, params object[] arguments)
-					=> Profile.WriteLog(format, arguments);
-
-				public void Set(object key, object value)
-					=> _dictionary.TryAdd(key, value);
-
-				public bool TryGet(object key, out object value)
-				{
-					var result = _dictionary.TryGetValue(key, out value);
-					if (!result)
-						return false;
-
-					if (_accessed.TryAdd(key, true))
-						Log("CACHE HIT: {0}", key);
-
-					return true;
-				}
-
-				public void Dispose()
-				{
-					foreach (var pair in _dictionary)
-					{
-						if (!_accessed.ContainsKey(pair.Key))
-							Log("CACHE UNUSED: {0}", pair.Key);
-
-						(pair.Value as IDisposable)?.Dispose();
-					}
-
-					_dictionary.Clear();
-
-					Log("CACHE DISPOSED");
-				}
-			}
-
-			sealed class Heap : IHeapOrCache
-			{
-				static ConcurrentBag<object> ActivateBag(object key)
-					=> new ConcurrentBag<object>();
-
-				readonly ConcurrentDictionary<object, object> _dictionary
-					= new ConcurrentDictionary<object, object>();
-				readonly Func<object, ConcurrentBag<object>> _activateBag = ActivateBag;
-
-				void Log(string format, params object[] arguments)
-					=> Profile.WriteLog(format, arguments);
-
-				ConcurrentBag<object> Get(object key)
-					=> (ConcurrentBag<object>)_dictionary.GetOrAdd(key, _activateBag);
-
-				public void Set(object key, object value)
-					=> Get(key).Add(value);
-
-				public bool TryGet(object key, out object value)
-				{
-					var result = Get(key).TryTake(out value);
-					Log("CACHE {0}: {1}", result ? "HIT" : "MISS", key);
-					return true;
-				}
-
-				public void Dispose()
-				{
-					foreach (var pair in _dictionary)
-					{
-						foreach (var value in ((ConcurrentBag<object>)pair.Value))
-						{
-							Log("CACHE UNUSED: {0}", pair.Key);
-							(value as IDisposable)?.Dispose();
-						}
-					}
-
-					_dictionary.Clear();
-
-					Log("HEAP DISPOSED");
-				}
-			}
-
-			public static HeapOrCache ActivateCache(Scheduler scheduler) => 
-				new HeapOrCache(scheduler, new Cache());
-
-			public static HeapOrCache ActivateHeap(Scheduler scheduler) => 
-				new HeapOrCache(scheduler, new Heap());
-
-			Scheduler _scheduler;
-			IHeapOrCache _heapOrCache;
-
-			HeapOrCache(Scheduler scheduler, IHeapOrCache container)
-			{
-				_scheduler = scheduler;
-				_heapOrCache = container;
-			}
-
-			void Log(string format, params object[] arguments)
-				=> Profile.WriteLog(format, arguments);
-
-			public object Get<T>(T key = default)
-				where T : IAnticipatable
-			{
-				if (!_heapOrCache.TryGet(key, out var value))
-					return key.Get();
-				
-				return value;
-			}
-
-			public void Anticipate<T>(T key = default)
-				where T : IAnticipatable
-			{
-				_scheduler.Schedule(() =>
-				{
-					try
-					{
-						var stopwatch = new Stopwatch();
-						stopwatch.Start();
-						_heapOrCache.Set(key, key.Get());
-						var ticks = stopwatch.ElapsedTicks;
-
-						Log("CASHED: {0}, ms={1}", key, TimeSpan.FromTicks(ticks).Milliseconds);
-					}
-					catch (Exception ex)
-					{
-						Log("EXCEPTION: {0}: {1}", key, ex);
-					}
-				});
-			}
-
-			public void Dispose()
-				=> _heapOrCache.Dispose();
-		}
-
-		readonly Scheduler _scheduler;
-		readonly HeapOrCache _cache;
-		readonly HeapOrCache _heap;
-		readonly CpuUsage __cpuStart;
-
-		internal Anticipator()
-		{
-			_scheduler = new Scheduler();
-			_heap = HeapOrCache.ActivateHeap(_scheduler);
-			_cache = HeapOrCache.ActivateCache(_scheduler);
-			__cpuStart = CpuUsage.Now;
-		}
-
-		internal void AnticipateAllocation<T>(T key = default)
-			where T : IAnticipatable
-			=> _heap.Anticipate(key);
-
-		internal object Allocate<T>(T key = default)
-			where T : IAnticipatable
-			=> _heap.Get(key);
-
-		internal void AnticipateValue<T>(T key = default)
-			where T : IAnticipatable
-			=> _cache.Anticipate(key);
-
-		internal object Get<T>(T key = default)
-			where T : IAnticipatable
-			=> _cache.Get(key);
-
-		public void Dispose()
-		{
-			_scheduler.Join();
-			_cache.Dispose();
-			_heap.Dispose();
-
-			var cpuNow = CpuUsage.Now;
-
-			Profile.WriteLog("CPU THN {0}", __cpuStart);
-			Profile.WriteLog("CPU NOW {0}", cpuNow);
-			Profile.WriteLog("CPU UTIL {0}%", cpuNow - __cpuStart);
-		}
-
 	}
 }
  
